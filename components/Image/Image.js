e59bd30d3f4e731099f71662a571fa9fb45b016f
Harr-Yates <hyates1@gmail.com>
2024-05-27 15:31:11 +0200

    deploy fix

--- components/Image/Image.js
+++ components/Image/Image.js
@@ -2,9 +2,10 @@ import NextImage from "next/image";
 import styles from "./Image.module.css";
 
 const Image = ({ width, maxWidth, ...rest }) => {
-  let widths = {};
-  width ? (widths["width"] = width) : "100%";
-  maxWidth ? (widths["maxWidth"] = maxWidth) : "100%";
+  const widths = {
+    width: width || "100%",
+    maxWidth: maxWidth || "100%"
+  };
 
   return (
     <div className={styles.imageContainer} style={widths}>
